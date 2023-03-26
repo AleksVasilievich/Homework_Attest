989a67badcf819513856642836334cf1610aad72
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2023-03-26 19:37:02 +0300

    Ошибки в тексте исправил

--- Edit.py
+++ Edit.py
@@ -4,7 +4,7 @@ from Delete import Delete
 
 class Edit:
     def edit(self):
-        print('Удалим редактируемую заметку на новую ')
+        print('Удалим редактируемую заметку и напишем новую ')
         de = Delete()
         de.delete_notes()
         se = Save()
